63e86d189bcfa30a8a9faaf71ee80aad0d26927c
Radim Daniel Pánek <rdpanek@gmail.com>
2012-05-21 08:16:09 +0200

    korektura

--- js/controllers/newsController.js
+++ js/controllers/newsController.js
@@ -1,11 +1,11 @@
 function NewsCtrl($scope) {
   $scope.news = [
-    {"content": "První čtvrtek v měsíci se koná neformální setkání webových vývojářů Spaghetti Code. Můžete se těšit na přednášku na téma logování od Miroslava Mrázka a na PHP5.4 od Vladislava Bělohradského",
+    {"content": "Každý první čtvrtek v měsíci se koná neformální setkání webových vývojářů - Spaghetti Code. Můžete se těšit na zajímavá témata přednášky.",
      "button_link": "http://srazy.info/spaghetti-code/2265",
 	 "button_target": "_blank",
 	 "button_name": "Zobrazit detail srazu"},
 
-    {"content": "Otevřely jsme první DeveloperHub v České Republice, které je pouze pro vývojáře. Přijďte se k nám podívat.",
+    {"content": "Otevřeli jsme první DeveloperHub v České Republice, který je pouze pro vývojáře. Přijďte se k nám podívat.",
 	 "button_link": "#contact",
 	 "button_target": "",
 	 "button_name": "Zobrazit adresu"}
